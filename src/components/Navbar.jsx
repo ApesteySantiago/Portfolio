2fbd8f8aeb0730c49b235835645e76e5c28d51f7
Apestey Santiago <apesteysantiago@hotmail.com>
2024-09-09 18:55:01 -0300

    Actualizacion de Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/LogoPorftolioApesteySantiago.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -6,7 +6,12 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return <nav className="mb-20 flex item-center justify-between py-6">
     <div className="flex flex-shrink-0 item-center">
-        <img src={logo} alt="logo" />
+        <img 
+          width={150}
+          height={150}
+          src={logo} 
+          alt="logo" 
+        />
     </div>
     <div className="m-8 flex item-center justify-center gap-4 texr-2xl">
         <FaLinkedin />
